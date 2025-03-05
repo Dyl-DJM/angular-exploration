6e7b693bbc36b8d5fbf9be6507ed3082282ab62e
Dyl-DJM <dyl.dejesusmilitar@gmail.com>
2025-03-05 17:06:28 +0100

    First component integration in the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './features/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [/*RouterOutlet,*/ NavbarComponent],
+  // templateUrl: './app.component.html',
+  template:'<app-navbar><app-navbar>',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
