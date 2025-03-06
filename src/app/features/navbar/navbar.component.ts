b89668049129d9db2553d1d6ada765a25ed886c9
Dyl-DJM <dyl.dejesusmilitar@gmail.com>
2025-03-06 22:00:49 +0100

    Added the ngStyle directive

--- src/app/features/navbar/navbar.component.ts
+++ src/app/features/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgClass } from '@angular/common';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [FormsModule, NgClass],
+  imports: [FormsModule, NgClass, NgStyle],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
