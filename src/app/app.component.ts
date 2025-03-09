30418ac0b5f321b7537942c02f824aba011aaaa2
Dyl-DJM <dyl.dejesusmilitar@gmail.com>
2025-03-09 18:47:52 +0100

    Added the @for structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,17 @@ import { NavbarComponent } from './features/navbar/navbar.component';
 import { AffichageDateComponent } from './pages/affichage-date/affichage-date.component';
 import { CardComponent } from "./features/products/card/card.component";
 import { OrderStatusComponent } from "./features/order-status/order-status.component";
+import { ProductsComponent } from "./features/products/products.component";
 
 @Component({
   selector: 'app-root',
-  imports: [/*RouterOutlet, NavbarComponent, AffichageDateComponent, CardComponent,*/ OrderStatusComponent],
+  imports: [/*RouterOutlet, NavbarComponent, AffichageDateComponent, CardComponent, OrderStatusComponent,*/ ProductsComponent],
   // templateUrl: './app.component.html',
   // template:'<app-navbar><app-navbar>',
   //template:'<app-affichage-date></app-affichage-date>',
   // template: '<app-card></app-card>',
-  template: '<app-order-status></app-order-status>',
+  // template: '<app-order-status></app-order-status>',
+  template: '<app-products></app-products>',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
--- src/app/features/products/products.component.ts
+++ src/app/features/products/products.component.ts
@@ -7,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
+	products = [
+		{
+			id: 1,
+			title: 'Product 1',
+			price: 100,
+			image: 'https://m.media-amazon.com/images/I/91t7YlWwGfL.jpg',
+			rating: {
+				rate: 0,
+				count: 0
+			},
+			description: 'Description 1',
+			category: 'Category 1'
+		}, 
 
+		{
+			id: 2,
+			title: 'Product 2',
+			price: 89,
+			image: 'https://m.media-amazon.com/images/I/918Ai9a893L._AC_UF1000,1000_QL80_.jpg',
+			rating: {
+				rate: 0,
+				count: 0
+			},
+			description: 'Description 2',
+			category: 'Category 2'
+		}
+	]
 }
