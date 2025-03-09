1ccb3f8f1aefbd66c1778b548aaf161d67450f07
Dyl-DJM <dyl.dejesusmilitar@gmail.com>
2025-03-09 18:14:39 +0100

    Added @switch structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,16 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './features/navbar/navbar.component';
 import { AffichageDateComponent } from './pages/affichage-date/affichage-date.component';
 import { CardComponent } from "./features/products/card/card.component";
+import { OrderStatusComponent } from "./features/order-status/order-status.component";
 
 @Component({
   selector: 'app-root',
-  imports: [/*RouterOutlet, NavbarComponent, AffichageDateComponent,*/ CardComponent],
+  imports: [/*RouterOutlet, NavbarComponent, AffichageDateComponent, CardComponent,*/ OrderStatusComponent],
   // templateUrl: './app.component.html',
   // template:'<app-navbar><app-navbar>',
   //template:'<app-affichage-date></app-affichage-date>',
-  template: '<app-card></app-card>',
+  // template: '<app-card></app-card>',
+  template: '<app-order-status></app-order-status>',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
