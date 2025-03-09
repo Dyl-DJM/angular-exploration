04e72cfda427a763e3755d0f7eb308cff0fe23b0
Dyl-DJM <dyl.dejesusmilitar@gmail.com>
2025-03-09 18:04:18 +0100

    Added @if and bootstrap config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './features/navbar/navbar.component';
 import { AffichageDateComponent } from './pages/affichage-date/affichage-date.component';
+import { CardComponent } from "./features/products/card/card.component";
 
 @Component({
   selector: 'app-root',
-  imports: [/*RouterOutlet, NavbarComponent,*/ AffichageDateComponent],
+  imports: [/*RouterOutlet, NavbarComponent, AffichageDateComponent,*/ CardComponent],
   // templateUrl: './app.component.html',
   // template:'<app-navbar><app-navbar>',
-  template:'<app-affichage-date></app-affichage-date>',
+  //template:'<app-affichage-date></app-affichage-date>',
+  template: '<app-card></app-card>',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
